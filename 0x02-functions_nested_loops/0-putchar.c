c6b6ba2e6ad8a0a291fd291ecf372d36315791c6
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-07 13:32:31 -0700

    Nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *main - prints the letter of _putchar
+ * main - prints the letter of _putchar
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
