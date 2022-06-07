f85db929bd47af86b7a24cde25f2a23ba9e5f5ee
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-07 10:00:41 -0700

    Nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,20 @@
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
+ * main:print letter of c.
  */
+
 int main(void)
 {
-    _putchar(95);
-    _putchar(112);
-    _putchar(117);
-    _putchar(116);
-    _putchar(99);
-    _putchar(104);
-    _putchar(97);
-    _putchar(114);
-    _putchar('\n');
-    return (0);
+           _putchar(95);
+           _putchar(112);
+           _putchar(117);
+
+           _putchar(116);
+           _putchar(99);
+           _putchar(104);
+           _putchar(97);
+           _putchar(114);
+           _putchar('\n');
+           return (0);
 }
