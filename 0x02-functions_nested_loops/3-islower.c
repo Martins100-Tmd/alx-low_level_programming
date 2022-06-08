dbd317984a3f8f771e64efaf8b3014c97077c4fe
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-08 00:21:41 -0700

    loop

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* Return: Always 0 (success)
-* main - Entry point
-*
-*_islower - Returns 1 if c is lowercase,Returns 0 otherwise
-*/
-
+ *_islower - Returns 1 if c is lowercase,Returns 0 otherwise
+ * c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
