03004f2af2f239a5d1154b51bb72ae5854d4d3c2
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-07 21:55:29 -0700

    loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
- * main - Prints _putchar
+ * print_alphabet - Prints lowercase alphabets
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
-	char alp = 'a';
-	while(alp <= 'z')
-	{
-		_putchar(alp);
-		alp++;
+        char alp = 'a';
+
+        while (alp <= 'z')
+        {
+                _putchar(alp);
+                alp++;
+	
 	}
 	_putchar('\n');
+
 }
