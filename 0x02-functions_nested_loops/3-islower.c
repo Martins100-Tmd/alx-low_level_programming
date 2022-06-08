a038bfc6fe08e92a859752e341c80562e0697f0b
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-07 23:59:17 -0700

    loop

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*islower - returns 1 when value is lower case and return 0 if else
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c<=122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
