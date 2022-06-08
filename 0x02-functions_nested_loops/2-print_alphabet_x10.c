e08160e2668080814bfb3fe9cd7d784442f21a41
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-07 23:21:06 -0700

    Nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * @print_alphabet_x10 - prints lowercase alphabet a-z 10 times
+ *print_alphabet_x10 - prints lowercase alphabet a-z 10 times
  * @c: The character to print
  *
  * Return: On success 1.
