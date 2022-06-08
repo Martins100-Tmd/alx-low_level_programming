ac609e83b7d2c07effd428a49f75754c0a809359
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-07 22:02:54 -0700

    nested looP

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet - Prints lowercase alphabets
- *
- * Return: Always 0 (Success)
- */
+* print_alphabet -Prints lowercase alpbates
+*
+*Return: Always 0 (success)
+*/
+
 void print_alphabet(void)
 {
-        char alp = 'a';
+	char alp = 'a';
 
-        while (alp <= 'z')
-        {
-                _putchar(alp);
-                alp++;
+	while (alp <= 'z')
+	{
+		_putchar(alp);
+		alp++;
 
-        }
-        _putchar('\n');
+	}
+	_putchar('\n');
 
 }
