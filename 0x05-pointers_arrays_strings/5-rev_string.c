208b5341f071c041b8b5bedd90234b05cea05c1c
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-13 08:59:09 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *rev_string - prints a string in reverse order
+ *@s: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+
+	for (i = i-1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}	
