67eff5cc08564c4eb45b7d18ff2f08917aa85ebb
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-22 06:30:17 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,18 +7,18 @@
  */
 int man(int n, int i)
 {
-	if (i * i < n)
-    	{
-        	return (man(n, i + 1));
-    	}
-    	else if (i * i == n)
-    	{
-        	return (i);
-    	}
-    	else
-    	{
-        	return (-1);
-    	}
+if (i * i < n)
+{
+	return (man(n, i + 1));
+}
+else if (i * i == n)
+{
+	return (i);
+}
+else
+{
+	return (-1);
+}
 }
 /**
  *_sqrt_recursion - evaluate sqrt
@@ -27,9 +27,9 @@ int man(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-	int i = 0;
+int i = 0;
 	
-	return (man(n, i));
+return (man(n, i));
 
-	return (0);
+return (0);
 }
