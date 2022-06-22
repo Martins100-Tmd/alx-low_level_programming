dfde3521df5ad364bfea019be47cdb7f5e0b1fca
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-22 06:06:01 -0700

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *man - evaluate function sqrt
+ *@i: interger
+ *@n: interger
+ *Return: evaluate sqrt
+ */
+
+int man(int n,int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i <= n)
+	{
+		return (man(n,i + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
+/**
+ *_sqrt_recursion - evaluate sqrt
+ *@n: interger
+ *Return: Sqrt_recursion
+*/
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+	man(n,i);
+}
