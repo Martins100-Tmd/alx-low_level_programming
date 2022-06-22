69517d9acc517e35d16a93d8c6f99ac909f1c5a0
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-22 05:09:46 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *strlen_recursion - print the length of a string
+ *@s - memory to be counted
+ *Return:void
+ */
+
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		return (0);
+	}
+	return (1+_strlen_recursion(s+1);
+}
