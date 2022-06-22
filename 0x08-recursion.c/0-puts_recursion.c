755159d861c0e2c79d7cc60155960a98a8f42eb4
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-22 03:42:06 -0700

    recursion

---
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *puts_recursion - prints a string
+ *@s - block of memory to be filled
+ *Return:void
+ */
+void _puts_recursion(char *s)
+{
+	if(s[i] = '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s+1);
+}
