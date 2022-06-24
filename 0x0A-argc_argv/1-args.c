4884a833dce8c1200b98f6462517f16e336b04d9
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-06-24 03:27:08 -0700

    argc & argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argc: number of arguments
+ *@argv: pointer to string with arguments
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	int i, sum, len;
+	if (argc >= 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			len = i;
+		}
+		sum = i - 1;
+		printf("%d",sum);
+	}
+	else {
+		return (0);
+	}
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc: number of arguments
+ *@argv: pointer to string with arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n",argv[i]);
+	}
+
+	return (0);
+}
