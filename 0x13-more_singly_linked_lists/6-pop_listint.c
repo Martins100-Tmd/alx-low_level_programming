8e6c0cf3aa9e8a2572d47c0119c192a2a3b75ea3
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-07-19 03:08:47 -0700

    Linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - delete a head node
+ *
+ *@head: pointer to list
+ *
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	int data;
+	listint_t *new;
+
+	if(head == NULL)
+		return (0);
+
+	new = *head;
+
+	*head = new->next;
+
+	data = new->n;
+
+	free(new);
+
+	return (data);
+}
