906a204e221b88ffd2b94e32e77c608fa6b5edfd
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-22 16:08:55 -0700

    Hashmap Huh

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "Hash.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *HASHTABLE;
+
+    HASHTABLE = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+    if (HASHTABLE == NULL)
+        return (NULL);
+    HASHTABLE->size = size;
+    HASHTABLE->array = malloc(sizeof(hash_node_t) * size);
+
+    if (HASHTABLE->array == NULL)
+        return (NULL);
+    int i = 0;
+    for (i; i < size; i++)
+    {
+        HASHTABLE->array[i] = NULL;
+    }
+    return HASHTABLE;
+}
