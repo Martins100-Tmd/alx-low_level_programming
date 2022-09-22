f190957188d6cb3e349165bf41d296ce46ba7411
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-22 16:36:07 -0700

    Hashmap Huh

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,14 +1,11 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - a function that creates a hash table
  *
  * @size: the size of the hash table array
  *
- * Return: Case of error - NULL.
- * 	else, return a pointer to the hash table
+ * Return: Case of error - NULL || pointer to a hash table
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *ht;
