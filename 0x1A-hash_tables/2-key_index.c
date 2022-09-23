e7d831158bcc5164b0e1a42968308face181487c
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-23 01:19:47 -0700

    Hashmap Huh

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
+
 /**
  * key_index - hash_key returning function
  * @key: key of the hashcode
  * @size: size of the hash table
- *
  * Return: unsigned long int
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
