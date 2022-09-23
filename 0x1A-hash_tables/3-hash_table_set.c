68b1227a0f85424623e28e881b6c6cfb2e372b04
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-23 02:12:30 -0700

    Hashmap Huh

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,9 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         ht->array[len]->value = strdup(value);
 	return (1);
     }
-
-    new_node = malloc(sizeof(hash_node_t));
-
+   new_node = malloc(sizeof(hash_node_t));
    if (new_node == NULL)
         return (0);
    new_node->key = strdup(key);
