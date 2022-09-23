c07fca14df531779e8bd602924f0936de87fc40b
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-23 02:35:46 -0700

    Hashmap Huh

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,24 +8,24 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-   unsigned long size, len;
-   hash_node_t *new_node;
+	unsigned long size, len;
+	hash_node_t *new_node;
 
-   if (ht == NULL || key == NULL || value == NULL)
-	return (0);
-   size = ht->size;
-   len = key_index((const unsigned char *)key, size);
-   if (ht->array[len] != NULL && strcmp(ht->array[len]->key, key) == 0)
-   {
-        ht->array[len]->value = strdup(value);
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+	size = ht->size;
+	len = key_index((const unsigned char *)key, size);
+	if (ht->array[len] != NULL && strcmp(ht->array[len]->key, key) == 0)
+	{
+		ht->array[len]->value = strdup(value);
+		return (1);
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[len];
+	ht->array[len] = new_node;
 	return (1);
-   }
-   new_node = malloc(sizeof(hash_node_t));
-   if (new_node == NULL)
-	return (0);
-   new_node->key = strdup(key);
-   new_node->value = strdup(value);
-   new_node->next = ht->array[len];
-   ht->array[len] = new_node;
-   return (1);
 }
