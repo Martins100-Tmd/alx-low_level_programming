04e55b88115b378f40dab0a84c6ccf1e43afa904
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-23 06:29:51 -0700

    Hashmap Huh

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,30 +9,32 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int hashcode, size;
-    hash_node_t *temp;
-    size = ht->size;
-    hashcode = key_index((const char *)key, size);
-    if (ht == NULL || key == NULL)
-        return (0);
-    temp = ht->array[hashcode];
-    if (temp != NULL && strcmp(temp->key, key) == 0)
-    {
-        return (temp->value);
-    }
-    else if (temp != NULL && strcmp(temp->key, key) != 0 && temp->next != NULL)
-    {
-        while (temp)
-        {
-            if (strcmp(temp->key, key) == 0)
-            {
-                return (temp->value);
-            }
-            temp = temp->next;
-        }
-    }
-    else
-    {
-        return (NULL);
-    }
-}
+	unsigned long int size, hashcode;
+
+	hash_node_t *temp;
+
+	size = ht->size;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	hashcode = key_index((const char *)key, size);
+	temp = ht->array[hashcode];
+
+	if (temp == NULL)
+		return (NULL);
+
+	if (temp && strcmp(temp->key, key) == 0)
+	{
+		return (temp->value);
+	}
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+		}
+		temp = temp->next;
+	}
+
+	return (NULL);
