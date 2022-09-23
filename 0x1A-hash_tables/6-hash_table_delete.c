cf30a4628a70a719749384f74a78c9561e2bba43
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-23 07:44:25 -0700

    Hashmap Huh

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,31 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hashtable
- * @ht: hashtable
- * Return: nothing
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-	hash_node_t *temp, *current_node;
-
-	for (i; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if(ht->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			current_node = ht->array[i];
-			while (current_node)
+			node = ht->array[i];
+			while (node != NULL)
 			{
-				temp = current_node->next;
-				free(current_node->key);
-				free(current_node->value);
-				free(current_node);
-				current_node = temp;
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
 			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
