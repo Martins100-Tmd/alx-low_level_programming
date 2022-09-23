cd00bf46e25c90b185e411a1aa80f91af140221b
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-23 01:12:01 -0700

    Hashmap Huh

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,35 @@
+#include "Hash.h"
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash, H;
+    int c;
+
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash += ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
+}
+/**
+ * key_index - hash_key returning function
+ * @key: key of the hashcode
+ * @size: size of the hash table
+ *
+ * Return: unsigned long int
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int Hash_pos;
+    if (key != NULL)
+    {
+        Hash_pos = hash_djb2((unsigned char *)key) % size;
+    }
+    return Hash_pos;
+}
