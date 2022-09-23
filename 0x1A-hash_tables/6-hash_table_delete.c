c9d7a0fbfa559987d95fcc9ac13e5e9b98862952
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-09-23 07:19:06 -0700

    Hashmap Huh

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hashtable
+ * @ht: hashtable
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int size, i = 0;
+	hash_node_t *temp, *current_node;
+
+	size = ht->size;
+	for (i; i < size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node)
+		{
+			temp = current_node->next;
+			free(current_node->key);
+			free(current_node->value);
+			free(current_node);
+			current_node = temp;
+		}
+	}
+	free(ht);
+	free(ht->array);
+}
