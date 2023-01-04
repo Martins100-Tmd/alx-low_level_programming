0386629139e48ab7dd40d384f1ea56907397be2c
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-04 02:18:01 -0800

    Linear Search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,17 +11,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-
+	size_t count = size;
 	if (!array)
 		return (-1);
-	int i = 0;
-
-	while (i < size)
+	while (--size)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-		i++;
+		printf("Value checked array[%ld] = [%d]\n", size, array[count - size]);
+		if (array[count - size] == value)
+			return (count - size);
 	}
 	return (-1);
 }
