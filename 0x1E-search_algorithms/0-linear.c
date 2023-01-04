dd799242ea0b28fc84da1f2e793f89ec4979db9e
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-04 01:52:11 -0800

    Linear Search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,5 @@
 #include "search_algos.h"
-
+#include <stdlib.h>
 /**
  * linear_search - search for a value in an array
  * using the Linear Search Algorithm
@@ -17,11 +17,23 @@ int linear_search(int *array, size_t size, int value)
 
 	while (i < size)
 	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
+			printf("Found %d at index: %d\n", array[i], i);
 			return (i);
 		}
 		i++;
 	}
 	return (-1);
+}
+
+int main()
+{
+	int array[] = {
+        	10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    	};
+    	size_t size = sizeof(array) / sizeof(array[0]);
+	linear_search(array, size, 42);
+	return (0);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,9 @@
-#ifndef SEARCH_ALGO_H
-#define SEARCH_ALGO_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 /**Standard Libraries*/
 #include <stdio.h>
 
 /**Prototypes*/
 int linear_search(int *array, size_t size, int value);
-
 #endif
