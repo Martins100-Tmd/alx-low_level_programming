7f72f99ba6e83de8db672beb4ab2f09bd2695f52
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-01-06 01:18:18 -0800

    Search Algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,19 +1,43 @@
 #include "search_algos.h"
 
+/**
+* binary_search - searches for a value in an array
+* using the binary search algo
+* @array: the array to be searched
+* @size: size of the array
+* @value: value to search for
+* Return: the index of the value if found or `-1` otherwise
+*/
 
 int binary_search(int *array, size_t size, int value)
 {
-	int L = 0, mid, R = size, result;
+	int *tmp = array;
+	int low = 0;
+	int i;
+	int high = size - 1;
+	int mid;
 
-	while (L < R)
+	if (!array)
+		return (-1);
+
+	while (low <= high)
 	{
-		mid = (L + R) / 2;
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+			if (i != high)
+				printf("%d, ", tmp[i]);
+			else
+				printf("%d", tmp[i]);
+		printf("\n");
 
-		if(array[mid] < value)
-			L = mid + 1;
-		else
-			R = mid;
+		mid = (low + high) / 2;
+		if (tmp[mid] < value)
+			low = mid + 1;
+		else if (tmp[mid] == value)
+			return (mid);
+		else if (tmp[mid] > value)
+			high = mid - 1;
 	}
-	result = array[L] ? L : -1;
-	return (result);
+
+	return (-1);
 }
